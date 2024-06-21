9f7b5c6b915a0daaf5183dc32dd6cc707db82be5
刘小平 <contact@liuxp.me>
2024-06-21 18:33:39 +0800

    调整统计代码。

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -69,7 +69,7 @@ export default defineConfig({
             }
         ],
         ['link', {rel: 'icon', href: '/favicon.ico'}],
-        ['script',{},`!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"CDqGooQbS8dts6eI",ck:"CDqGooQbS8dts6eI",hashMode:true});`]
+        ['script',{},`!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("//a.com/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"CDqGooQbS8dts6eI",ck:"CDqGooQbS8dts6eI",hashMode:true});`]
     ],
     assetsDir: 'public',
     srcDir: 'src',
